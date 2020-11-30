9788c4bd15380002ad88fe225ee0c7e18e033493
Ana Korunovic <korunovicana9@gmail.com>
2020-11-30 21:30:55 +0100

    ana

--- js/script.js
+++
@@ -1,9 +0,0 @@
-function prikazi_confirm() {
-    var r = confirm("Da li ste sigurni")
-    if (r == true) {
-        document.write("Pritisnuli ste OK!")
-    } else {
-        document.write("Pritisnuli ste Cancel!")
-    }
-}
-document.getElementById('dugmeSubmit').addEventListener('click', prikazi_confirm);
