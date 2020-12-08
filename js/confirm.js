0de34b1e0cc88b755f38839e566b032ce88206ba
Ana Korunovic <korunovicana9@gmail.com>
2020-12-08 19:04:18 +0100

    ana

--- js/confirm.js
+++ js/confirm.js
@@ -7,21 +7,18 @@ function fja_confirmNe(){
 }
 
 function validacijaForme() {
-    if( document.getElementsByName('ime').value == "" ) {
-        alert("Unesite ime i prezime ");
-       
-        return false;
-     }
-     if( document.getElementsByName('email').value == "" ) {
-        alert("Unesite email");
-       
-        return false;
-     }
-     if( document.getElementsByName('comment').value == "" ) {
-        alert("Unesite komentar");
-        
-        return false;
-     }
-     return true ;
-    
+   if (document.getElementsByClassName('ime').value == "") {
+      alert("Unesite ime i prezime ");
+
+      return false;
+   }else if (document.getElementsByName('email').value == "") {
+      alert("Unesite email");
+
+      return false;
+   }else if (document.getElementsByName('comment').value == "") {
+      alert("Unesite komentar");
+
+      return false;
+   }else return true;
+
 }
